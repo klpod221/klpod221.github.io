f6b4a8362d38a68c915b6f725a09ee9a797e5670
klpod221 <klpod221@gmail.com>
2024-09-09 16:24:43 +0700

    Update project links and add new projects to projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,11 @@
-import devtoolkit from "../assets/images/devtoolkit.png";
+import devtoolkit from "../assets/images/projects/devtoolkit.png";
+import calculator from "../assets/images/projects/calculator.png";
+import _2048 from "../assets/images/projects/2048.png";
+import klpod0s from "../assets/images/projects/klpod0s.png";
+import getFacebookMembers from "../assets/images/projects/get-facebook-members-extension.png";
+import linkedinConnectBot from "../assets/images/projects/linkedin-connect-bot.png";
+import miniProjects from "../assets/images/projects/mini-projects.png";
+import moreProjects from "../assets/images/projects/more.png";
 
 export const projects = [
   {
@@ -8,4 +15,49 @@ export const projects = [
     description:
       "A collection of tools for developers. Includes a Code compiler, Image Tools, Web Tools, and more.",
   },
+  {
+    title: "Calculator",
+    image: calculator,
+    description: "A simple calculator app.",
+    link: "https://klpod221.site/code-tournament-calculator/",
+  },
+  {
+    title: "2048",
+    image: _2048,
+    description: "Clone of 2048 classic game",
+    link: "https://klpod221.site/2048",
+  },
+  {
+    title: "klpod0s",
+    image: klpod0s,
+    description:
+      "Aesthetic, dynamic and minimal dots for Arch Linux + Hyprland",
+    link: "https://klpod221.site/klpod0s",
+  },
+  {
+    title: "Get members of Facebook group",
+    image: getFacebookMembers,
+    description:
+      "Chrome Extension to get member of Facebook group and export to xlsx file",
+    link: "https://klpod221.site/chrome-extension-get-member-of-facebook-group",
+  },
+  {
+    title: "Linkedin Connect Bot",
+    image: linkedinConnectBot,
+    description:
+      "Simple Python + Selenium script that help auto send Linkedin connect request",
+    link: "https://klpod221.site/linkedin-connect-bot",
+  },
+  {
+    title: "Mini Projects",
+    image: miniProjects,
+    description: "A collection of mini projects",
+    link: "https://klpod221.site/mini-projects",
+  },
+  {
+    title: "More Projects ...",
+    image: moreProjects,
+    description: "More projects on my Github",
+    link: "https://github.com/klpod221?tab=repositories",
+  }
 ];
