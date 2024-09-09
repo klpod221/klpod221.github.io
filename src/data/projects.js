42160bba137df358a1f7292ae408f5632ecd009b
klpod221 <klpod221@gmail.com>
2024-09-09 15:48:43 +0700

    Update project link in projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,8 +4,8 @@ export const projects = [
   {
     title: "DevToolkit",
     image: devtoolkit,
-    link: "https://dev.klpod221.site/",
+    link: "https://devtool.klpod221.site/",
     description:
-      "A collection of tools for developers. Includes a Code compiler, Image Tools, and more.",
+      "A collection of tools for developers. Includes a Code compiler, Image Tools, Web Tools, and more.",
   },
 ];
