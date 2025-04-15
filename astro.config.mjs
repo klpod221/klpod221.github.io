8e7eafefeb53cdf1db283ff21b468ffb44bf388f
klpod221 <klpod221@gmail.com>
2025-04-15 13:28:13 +0700

    refactor: update dependencies and improve project structure
    - Updated dependencies in package.json for better performance and compatibility.
    - Refactored ProjectList component to use MySlider for improved slider functionality.
    - Simplified SkillList component rendering logic for better performance.
    - Added global CSS import in BaseLayout for consistent styling.
    - Enhanced Footer and Header components with MyLink for better link management.
    - Expanded skill set in index.astro to include additional technologies.
    - Removed obsolete tailwind.config.mjs and migrated styles to global.css.
    - Introduced MyLink and MySlider components for reusable link and slider functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://klpod221.com',
-  integrations: [
-    tailwind(),
-    icon({
-      iconDir: {
-        mdi: "*", // Include all icons from the Material Design Icons set
-        tabler: "*", // Include all icons from the Tabler Icons set
-        "fa6-brands": "*", // Include all icons from the Font Awesome 6 Brands set
-        "simple-icons": "*", // Include all icons from the Simple Icons set
-      },
-    }),
-  ],
+  site: "https://klpod221.com",
+  integrations: [icon()],
+  vite: {
+    plugins: [
+      tailwindcss({
+        config: "./tailwind.config.mjs",
+      }),
+    ],
+  },
 });
